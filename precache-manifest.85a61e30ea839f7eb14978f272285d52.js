7c5409a11c0b62b8ee4b487c495f870e35deaa3d
Santhosh <bgj.santhosh@gmail.com>
2020-01-29 17:19:28 +0530

    Updates

---
+++ precache-manifest.85a61e30ea839f7eb14978f272285d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539b337cde7338258eca3cc21ba22331",
+    "url": "/tmdbsearch/index.html"
+  },
+  {
+    "revision": "04fb6fe2c6843c3fd842",
+    "url": "/tmdbsearch/static/css/main.fe159492.chunk.css"
+  },
+  {
+    "revision": "a2d9c6a3734f6d413493",
+    "url": "/tmdbsearch/static/js/2.85511838.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tmdbsearch/static/js/2.85511838.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04fb6fe2c6843c3fd842",
+    "url": "/tmdbsearch/static/js/main.2343d268.chunk.js"
+  },
+  {
+    "revision": "fbb7fd0782ec6d2604a0",
+    "url": "/tmdbsearch/static/js/runtime-main.779e507d.js"
+  }
+]);
