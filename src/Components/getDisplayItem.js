cc4ab17a214f79555846523061fd81289e24c280
Santhosh <bgj.santhosh@gmail.com>
2020-02-04 15:17:51 +0530

    add favicon

--- src/Components/getDisplayItem.js
+++ src/Components/getDisplayItem.js
@@ -6,10 +6,5 @@ export const getDisplayItem = async (id, type) => {
   let res = await fetch(fetchURL);
 
   let data = await res.json();
-
-  // const movie = `https://api.themoviedb.org/3/movie/359724?api_key=${API_KEY}&language=en-US&query=${search}&page=1`;
-  // let res1 = await fetch(movie);
-  // let data1 = await res1.json();
-  // console.log(data1);
   return data;
 };
